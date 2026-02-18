e034b9effcffcee9bb3306f6f5e454cc5cc3fc52
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-02-18 17:43:34 +0530

    api calling fetch and axios

---
+++ API-Calling/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+
+const App = () => {
+  async function getData(){
+    console.log('Getting data...')
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+     const data = await response.json()
+    console.log(data)
+  }
+
+  async function axiosApiCall() {
+
+    console.log('Getting data with axios...')
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    console.log(response.data)
+  }
+  return (
+    <div>
+      <button onClick={axiosApiCall}>Get Data!</button>
+    </div>
+  )
+}
+
+export default App
